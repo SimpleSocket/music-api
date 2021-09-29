ab951946bf395460dfc71b287a65dd68e7778134
SimpleSocket <tomjurevic@gmail.com>
2021-09-29 13:26:58 +0300

    [master] remove blank line, optimized imports

--- backend/src/main/java/net/unknown/musicapi/controllers/ArtistApi.java
+++ backend/src/main/java/net/unknown/musicapi/controllers/ArtistApi.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.Optional;
 
 @RestController
